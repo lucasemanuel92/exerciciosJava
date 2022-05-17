bcae05a07f11f3e0458e71d749773fdd6d5f14d9
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2022-05-17 15:00:09 -0300

    Add files via upload

---
+++ ResolucaoPessoal.java
@@ -0,0 +1,26 @@
+package fundamentos;
+
+public class ResolucaoPessoal {
+
+	public static void main(String[] args) {
+
+		// Superior 1 
+		int a = (6 * (3 + 2)); 
+		double b =  Math.pow(a, 2);
+		int c = 3 * 2;
+		
+		int d = (1 - 5) * (2 - 7) / 2;
+		double e = Math.pow(d, 2);
+	
+		double f = Math.pow(b / c - e, 3);		
+		
+		double g = Math.pow(10, 3);
+		
+		System.out.println(f / g);
+		
+		
+		
+		
+	}
+
+}
